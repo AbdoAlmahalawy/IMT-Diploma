fc827bd7e20b1dd7204a67166282b67b06ac24d9
AbdoAlmahalawy <abdelrahmanalmahalawy@gmail.com>
2017-02-10 17:23:47 +0200

    CV

---
+++ test/courses/embedded system/Sessions/session(1)/Ass.1/cv/CV.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(void)
+{
+printf("-My name is Abdel-Rahman Salah. \n ");
+
+printf("-Personal Information:\n    Date of Birth: 04/7/1993.\n    Place of Birth: UAE.\n    Gender: Male.\n    Marital Status: Single.\n    Nationality: Egyptian.\n    Resident: Helwan, Egypt.\n    Military service: Exempted.\n");
+
+printf("-Education:\n   Degree : Graduated.\n    University : Modern Academy.\n    Faculty : Faculty of engineering.\n    Joining Year : 2011.\n    Graduated Year : 2016    Department : Communication.\n    Grade : Very Good with honor rank (79.45%).\n    Graduation Project Grade : Excellent.\n");
+
+printf("-Graduation Project Name : E-Clinic Solution (it is the use of telecommunication and information technologies to provide clinical health care at a distance. By using medical sensors the patient can Send his measurements from an account that he created from the ECS website, He can make online session with his doctor and sending to him text, picture and video. The project consists of Hardware and Software (Database & Website & Mobile Application & Video chat).\n");
+
+}
